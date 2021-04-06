b910ff35e39d6c564aeee45cc3ad06ce79c739fd
Abdullah Al Masum <abdullahmasum6035@gmail.com>
2021-04-06 23:54:16 +0600

    Day 00

--- Day00/Standard template.asm
+++ Day00/Standard template.asm
@@ -1,6 +1,6 @@
 ;Day 00_________date-06 april,2021
 ;Standard Template of any assembly language
-
+include 'emu8086.inc'
 .model small	;Code in one segment, and data in one segment
 .stack 100h		;stack size is declared
 
@@ -13,7 +13,12 @@ main proc
 	mov ax,@data	;all the addressess of data segment are copied in accumalator register 
 	mov ds,ax    ;in near future we may use ax register so we have to assign all our addresses into data segment register (ds) 
 
-	;Write your code here
+	;Some Library function
+	putc 'any_charachter to show in console'
+	print "Your String here to show in console"
+	printn      ;For new Line
+	;Write code here
+	
 
 	mov ah,4ch
 	int 21h		;terminate with return code
