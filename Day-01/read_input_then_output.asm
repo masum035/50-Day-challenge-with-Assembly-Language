336977288e37970b7fb7844cc8e2d1bf7ce3058f
Abdullah Al Masum <abdullahmasum6035@gmail.com>
2021-04-07 17:44:48 +0600

    Day-01 (07/04/21)

--- Day-01/read_input_then_output.asm
+++ Day-01/read_input_then_output.asm
@@ -13,6 +13,12 @@ n_line db 0ah,0dh,"$"   ;for new line
 var_int db ?
 var_char db ?
 var_str dw ?
+arr1 DD 100 DUP(0)    ; Declare 100 4-byte words starting at location arr1, all initialized to 0
+str1 dw "Give Input String : $"
+str2 dw "Given input is : $"  
+str3 dw "Total charachters in given string : $"
+arr db 30 dup('$')    ; Declare 30 4-byte words starting at location arr, all initialized to $(null charachter)  
+var_count db ?
 
 .code 
     
@@ -22,7 +28,7 @@ main proc
 
 	;Write your code here
   	;First let's take integer input
-  	mov ah,1	;For number input
+  	mov ah,1	;For number/charachter input
   	int 21h
 	
   	;sub al,48  
@@ -58,24 +64,45 @@ main proc
     mov dl,var_char
     
     mov ah,2
-    int 21h  
-    
+    int 21h
+      
+    printn
     ;Now let's String input 
+    lea dx,str1   ; For str1 print
+    mov ah,09
+    int 21h
     
-    ;mov dx,offset var_str 
-    ;mov ah,3fh
-    ;int 21h 
+    printn
     
-    ;lea dx,n_line
-    ;mov ah,9
-    ;int 21h 
+    mov ah,10     ; For string input
+    lea dx,arr    ; Now pointing dx(data register) to starting address of arr 
+    ;mov arr,29   ; (It's not necessary) It's just a constraint to remain in specified bound,cause here arr is 30,So input can be <= max size 
+    int 21h
     
-    ;mov dx,offset var_str
-    ;mov ah,9h
-    ;int 21h
+    printn
+    
+    lea dx,str2   ; For str2 print
+    mov ah,09
+    int 21h 
+    
+    lea dx,arr+2  ; (why arr+2 ?)cause arr[0] stores declared size of arr and arr[1] stores counter of total inputed charachters.So arr starts from arr[2]
+    mov ah,09
+    int 21h    
+    
+    printn
+    
+   ; lea dx,str3
+   ; mov ah,09
+   ; int 21h
+    
+   ; mov al,arr[1] ;
+   ; mov dl,al
+   ; mov ah,02
+   ; int 21h
+   
+    
      
 	mov ah,4ch
 	int 21h		;terminate with return code
 main endp 
 end main
-
