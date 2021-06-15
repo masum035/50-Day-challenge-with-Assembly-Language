cf5e8fd1e02c48c61e858bdfd008044d20ce283e
Abdullah Al Masum <abdullahmasum6035@gmail.com>
2021-06-16 03:13:25 +0600

    Day-40 (10/06/21)

---
+++ Day-40/my_answer_in_Stackoverflow.asm
@@ -0,0 +1,73 @@
+; answer link: https://stackoverflow.com/a/67128604/13939591
+include 'emu8086.inc' 
+.model small
+.stack 100h
+
+.data
+n_line db 0ah,0dh,"$"  ;for new line
+msg1 db "input : $" 
+msg2 db 0ah,0dh,"Outpur : $"
+msg3 db 0ah,0dh,"invalid input given. $"  
+i db ?
+
+.code
+main proc
+    mov ax,@data
+    mov ds,ax
+
+@infinite:  
+    mov i,0d           ;for infinite process i=0 at initialization
+         
+    lea dx,msg1   
+    mov ah,09
+    int 21h 
+
+                       ;clear bx register
+    xor bx,bx          ;XOR operation with self will always produce 0    
+        
+    
+    mov ah,1
+    @input:            
+        int 21h        
+        
+        cmp al,50      ;check for input only 0 and 1
+        jge @invalid
+        cmp al,48
+        jl @invalid
+                       ;65d = 01000001 
+        and al,0Fh     ;0Fh = 00001111
+                       ;AND = 01000001
+        shl bx,1
+        or bl,al       ;store al data at LSB of BL register 
+                 
+        inc i
+        cmp i,8d
+        jl @input
+        jge @next_step
+    
+    @next_step:   
+        lea dx,msg2   
+        mov ah,9
+        int 21h
+         
+        mov dl,bl       ;print Ascii value
+        mov ah,2
+        int 21h  
+        
+        lea dx,n_line   ;for new line
+        mov ah,9
+        int 21h  
+        
+        jmp @infinite
+            
+         
+@stop:       
+    mov ah,4ch
+    int 21h
+    main endp
+@invalid:
+    lea dx,msg3
+    mov ah,9
+    int 21h
+    jmp @stop
+end main
